ac919eda0b82d4680b3538724edaaa4899df3c28
Sophie Dubois <sophiemargueritedubois@gmail.com>
2019-10-01 21:04:17 -0400

    Added the functionality to cross off completed items off the list

--- src/components/Todoitem.js
+++ src/components/Todoitem.js
@@ -2,10 +2,29 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export class Todoitem extends Component {
+    getStyle = () => {
+      return {
+          backgroundColor: '#f4f4f4',
+          padding: '10px',
+          borderBottom: '1px #ccc dotted',
+          textDecoration: this.props.todo.completed ? 'line-through' : 'none'
+      }
+    }
+
+    markComplete = (e) => {
+        console.log(this.props);
+       
+    }
+
     render() {
+        const {id, title} = this.props.todo;
+
         return (
-            <div>
-                <p>{this.props.todo.title}</p>
+            <div style={this.getStyle()}>
+                <p>
+                    <input type="checkbox" onChange={this.props.markComplete.bind(this, id)}/> {' '}
+                    {title}
+                </p>
             </div>
         )
     }
